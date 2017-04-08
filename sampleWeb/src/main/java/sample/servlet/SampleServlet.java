580d3c245cdc9d661e157e2b0e38a41b7becad57
Pradeepkumar <cgpradeepkumar@gmail.com>
2017-04-08 06:38:24 +0530

    initial

---
+++ sampleWeb/src/main/java/sample/servlet/SampleServlet.java
@@ -0,0 +1,19 @@
+package sample.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by pradeepkumar on 8/4/17.
+ */
+public class SampleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        System.out.println("Welcome " + name);
+    }
+}
