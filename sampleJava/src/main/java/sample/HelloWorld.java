32169c593baeb965add7fa950ade11c5313f2b6a
Pradeepkumar <cgpradeepkumar@gmail.com>
2017-07-04 22:35:28 +0530

    working initial copy

---
+++ sampleJava/src/main/java/sample/HelloWorld.java
@@ -0,0 +1,10 @@
+package sample;
+
+/**
+ * Created by pradeepkumar on 4/7/17.
+ */
+public class HelloWorld {
+    public static void main(String... arg) {
+        System.out.println("Hello World!");
+    }
+}
