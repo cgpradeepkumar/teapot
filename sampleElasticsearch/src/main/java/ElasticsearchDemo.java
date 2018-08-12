d8c742b2bc080b5a2868b53926de6cc7e1013c48
pradeepkumar <123password>
2018-08-12 14:47:23 +0530

    create index

--- sampleElasticsearch/src/main/java/ElasticsearchDemo.java
+++ sampleElasticsearch/src/main/java/ElasticsearchDemo.java
@@ -1,4 +1,7 @@
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
@@ -21,6 +24,27 @@ public class ElasticsearchDemo {
 
             response = client.performRequest("GET", "/classroom", Collections.singletonMap("pretty", "true"));
             System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //index a document
+        HttpEntity entity = new NStringEntity(
+                "{\n" +
+                        "    \"user\" : \"kimchy\",\n" +
+                        "    \"post_date\" : \"2009-11-15T14:12:12\",\n" +
+                        "    \"message\" : \"trying out Elasticsearch\"\n" +
+                        "}", ContentType.APPLICATION_JSON);
+
+        try {
+            Response indexResponse = client.performRequest(
+                    "PUT",
+                    "/twitter/tweet/1",
+                    Collections.<String, String>emptyMap(),
+                    entity);
+
+            System.out.println(indexResponse);
         } catch (IOException e) {
             e.printStackTrace();
         }
