89e8fc5bb5457973d1e82c4512e1d9a850bf2ae5
Pradeepkumar <cgpradeepkumar@gmail.com>
2019-04-20 17:19:36 +0100

    added cors permission for all

--- libraryRestApi/src/main/java/in/library/application/LibraryWebApplication.java
+++ libraryRestApi/src/main/java/in/library/application/LibraryWebApplication.java
@@ -2,6 +2,10 @@ package in.library.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by pradeepkumar on 10/2/18.
@@ -12,5 +16,15 @@ public class LibraryWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryWebApplication.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+    	return new WebMvcConfigurerAdapter() {
+    		@Override
+    		public void addCorsMappings(CorsRegistry registry) {
+    			registry.addMapping("/**").allowedOrigins("*");
+    		}
+    	};
     }
 }
