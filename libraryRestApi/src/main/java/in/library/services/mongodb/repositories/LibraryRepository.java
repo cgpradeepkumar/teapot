87242a61c83ca5f7118ee06b947ec844c93ec4e4
pradeepkumar <cgpradeepkumar@gmail.com>
2024-09-07 16:00:41 +0100

    upgrading springboot and gradle versions

--- libraryRestApi/src/main/java/in/library/services/mongodb/repositories/LibraryRepository.java
+++
@@ -1,7 +0,0 @@
-package in.library.services.mongodb.repositories;
-
-import in.library.services.mongodb.documents.Item;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LibraryRepository extends MongoRepository<Item, String> {
-}
